0a5ccfa36bd0a32de5b3da3309e00b2125b7fd92
Marco Antônio Dal Vesco <133788104+MarcoDV47@users.noreply.github.com>
2024-10-12 11:52:32 -0300

    Update index.js

--- index.js
+++ index.js
@@ -9,10 +9,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 const db = new pg.Client({
-  user: "postgres",
+  user: "/* Usuário */",
   host: "localhost",
   database: "books",
-  password: "CarlosVSTim!",
+  password: "/* Senha */",
   port: 5432
 });
 
